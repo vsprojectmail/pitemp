354508d37d0915adb48501dc514b0f985a9bd920
vsproject <vsprojectmail@gmail.com>
2016-07-21 14:18:04 +0000

    working synctemp credentials

--- synctemp.py
+++ synctemp.py
@@ -2,5 +2,5 @@ from simple_salesforce import Salesforce
 from datetime import datetime
 dt = datetime.now()
 from random  import  randint
-sf = Salesforce(username='vshighadmin@gmail.com', password='qwertyu1love', security_token='mQOWJP4u9dTAt2vi0HdNiNeS3')
+sf = Salesforce(username='vsadmin2@gmail.com', password='ymHT3e3xMGyHMqo1', security_token='Sr99CXKsdY3ib90Vo0gdTe6T')
 print sf.Temperature__c.create({'Computer_Name__c':'CoolCat','Location__c':'Home','Temp__c':str(randint(70,99)), 'TimeOfReading__c':dt.isoformat("T")})
